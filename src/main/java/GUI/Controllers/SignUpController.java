95bb817095a9caa4f8987b25539ee11fd452af2a
WDCT-Wren <wrenctulio@gmail.com>
2026-04-01 22:22:09 +0800

    feat: Initial scene and styling of Sign up scene

---
+++ src/main/java/GUI/Controllers/SignUpController.java
@@ -0,0 +1,38 @@
+package GUI.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.ProgressBar;
+
+public class SignUpController {
+
+    @FXML
+    private PasswordField AuthTextField;
+
+    @FXML
+    private Button UnlockButton;
+
+    @FXML
+    private ProgressBar masterKeyStrength;
+
+    @FXML
+    private Button signInButton;
+
+    @FXML
+    void switchToMenuScene(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void setMasterKeyStrength() {
+
+    }
+
+    @FXML
+    void switchToSignInScene(ActionEvent event) {
+
+    }
+
+}
